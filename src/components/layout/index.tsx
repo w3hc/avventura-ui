c9578529a2433f2072c9894e1aefa26157d54959
julienbrg <julien@strat.cc>
2024-08-01 16:49:38 +0200

    add header conditional rendering

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -2,16 +2,20 @@ import { Web3Modal } from '../../context/web3modal'
 import { ReactNode } from 'react'
 import { Box, Container } from '@chakra-ui/react'
 import { Header } from './Header'
+import { useRouter } from 'next/router'
 
 interface Props {
   children?: ReactNode
 }
 
 export default function RootLayout({ children }: Props) {
+  const router = useRouter()
+  const isHomePage = router.pathname === '/' || router.pathname === '/index'
+
   return (
     <Web3Modal>
       <Box margin="0 auto" minH="100vh">
-        <Header />
+        {isHomePage && <Header />}
         <Container maxW="container.lg">{children}</Container>
       </Box>
     </Web3Modal>
